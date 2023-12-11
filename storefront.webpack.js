7c6686150b59dc82adcfaecd96c4fe9efb473620
matheusgnreis <matheus@e-com.club>
2023-12-11 15:50:33 -0300

    chore: clone store griffe chick with atacado

---
+++ storefront.webpack.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/CartItem.html': path.resolve(__dirname, 'template/js/custom-js/html/CartItem.html'),
+      './js/CartItem.js': path.resolve(__dirname, 'template/js/custom-js/js/CartItem.js'),
+      './js/LoginBlock.js': path.resolve(__dirname, 'template/js/custom-js/js/LoginBlock.js'),
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
+      './html/ProductVariations.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductVariations.html'),
+      './js/ProductVariations.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductVariations.js'),
+      './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
+      './html/LoginModal.html': path.resolve(__dirname, 'template/js/custom-js/html/LoginModal.html'),
+      './html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/html/TheAccount.html'),
+      './js/TheAccount.js': path.resolve(__dirname, 'template/js/custom-js/js/TheAccount.js'),
+      './js/QuantitySelector.js': path.resolve(__dirname, 'template/js/custom-js/js/QuantitySelector.js')
+    }
+  }
+})
